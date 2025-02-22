c0b3705c3e3bba770ea9bfbe7a59b21d8f2801ac
green <grzx2210@163.com>
2025-02-22 09:25:56 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,16 +1,19 @@
 from .api_config import LLMRemoteAPIConfig
 from .api_chat import LLMRemoteAPIChat
 from .LLMThinkParser import LLMThinkParser
+from .manual_config_loader import LLMRemoteAPIManualConfig
 
 
 NODE_CLASS_MAPPINGS = {
     "LLMRemoteAPIConfig": LLMRemoteAPIConfig,
+    "LLMRemoteAPIManualConfig": LLMRemoteAPIManualConfig
     "LLMRemoteAPIChat": LLMRemoteAPIChat,
-     "LLMThinkParser": LLMThinkParser
+    "LLMThinkParser": LLMThinkParser
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "LLMRemoteAPIConfig": "🌐 API 配置加载",
+    "LLMRemoteAPIConfig": "🌐 LLM远程API配置文件加载",
+    "LLMRemoteAPIManualConfig": "🌐 LLM远程API手动配置"
     "LLMRemoteAPIChat": "💬 API 对话处理",
     "LLMThinkParser": "📖 Think标签解析器"
 }
